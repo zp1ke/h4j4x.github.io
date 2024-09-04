5d95f14c9878ab06ca1b82c596a984602b97f622
Sp1k_e <miche.atucha@gmail.com>
2024-09-03 19:04:08 -0500

    bone bridge web

--- public/bone-bridge/flutter_bootstrap.js
+++ public/bone-bridge/flutter_bootstrap.js
@@ -6,7 +6,7 @@ Read more: https://developer.mozilla.org/en-US/docs/Web/Security/Secure_Contexts
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"c9b9d5780da342eb3f0f5e439a7db06f7d112575","builds":[{"compileTarget":"dart2wasm","renderer":"skwasm","mainWasmPath":"main.dart.wasm","jsSupportRuntimePath":"main.dart.mjs"},{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
+_flutter.buildConfig = {"engineRevision":"c9b9d5780da342eb3f0f5e439a7db06f7d112575","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
 
 
 const searchParams = new URLSearchParams(window.location.search);
@@ -19,7 +19,7 @@ loading.textContent = 'Loading...';
 _flutter.loader.load({
   config: userConfig,
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1559137994",
+    serviceWorkerVersion: "2663841564",
   },
   onEntrypointLoaded: async function(engineInitializer) {
     loading.textContent = 'Initializing engine...';
